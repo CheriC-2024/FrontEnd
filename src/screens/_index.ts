8b529e2017bc0731b891434088273eb1bf30fddc
Minji Kim <minjikim2000@hotmail.com>
2024-05-29 23:39:25 +0900

    feat: add navigation bar screens

---
+++ src/screens/_index.ts
@@ -0,0 +1,5 @@
+export { default as HomeScreen } from './HomeScreen';
+export { default as SearchScreen } from './SearchScreen';
+export { default as ExhibitScreen } from './ExhibitScreen';
+export { default as CollectingScreen } from './CollectingScreen';
+export { default as MyCheriCScreen } from './MyCheriCScreen';
