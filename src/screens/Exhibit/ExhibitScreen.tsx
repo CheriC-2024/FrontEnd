ab29df94a4c2a1387daef7cb205d59b23849b72e
Minji Kim <minjikim2000@hotmail.com>
2024-06-01 16:26:37 +0900

    bug: progress bar error resolved #9

--- src/screens/Exhibit/ExhibitScreen.tsx
+++ src/screens/Exhibit/ExhibitScreen.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import { TouchableOpacity, Text } from 'react-native';
-import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import {
+  useNavigation,
+  useRoute,
+  RouteProp,
+  CommonActions,
+} from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import CollectionSelect from './CollectionSelect';
 import ArtworkSelect from './ArtworkSelect';
@@ -10,6 +15,7 @@ import ArtworkInfoSetting from './ArtworkInfoSetting';
 import DescriptionSetting from './DescriptionSetting';
 import CoverSetting from './CoverSetting';
 import FinishSetting from './FinishSetting';
+import { useProgressBar } from '../../components/ProgressBarContext';
 
 type RootStackParamList = {
   MainTabs: undefined;
@@ -30,10 +36,15 @@ const Container = styled.View`
 const ExhibitScreen: React.FC = () => {
   const navigation = useNavigation<ExhibitScreenNavigationProp>();
   const route = useRoute<ExhibitScreenRouteProp>();
-  const [step, setStep] = useState(route.params?.step || 0);
-  const [selectedThemes, setSelectedThemes] = useState<string[]>(
-    route.params?.selectedThemes || [],
-  );
+  const { setStep } = useProgressBar();
+  const [step, setLocalStep] = useState(route.params?.step || 0);
+
+  useEffect(() => {
+    if (route.params?.step !== undefined) {
+      setLocalStep(route.params.step);
+      setStep(route.params.step); // Ensure ProgressBar step is updated
+    }
+  }, [route.params?.step]);
 
   useEffect(() => {
     navigation.setOptions({
@@ -50,14 +61,9 @@ const ExhibitScreen: React.FC = () => {
     });
   }, [navigation, step]);
 
-  useEffect(() => {
-    if (route.params?.selectedThemes) {
-      setSelectedThemes(route.params.selectedThemes);
-    }
-  }, [route.params?.selectedThemes]);
-
   const goToNext = () => {
     if (step < 6) {
+      setLocalStep(step + 1);
       setStep(step + 1);
     } else {
       navigation.navigate('MainTabs');
@@ -66,9 +72,17 @@ const ExhibitScreen: React.FC = () => {
 
   const goToPrev = () => {
     if (step > 0) {
+      setLocalStep(step - 1);
       setStep(step - 1);
     } else {
-      navigation.navigate('MainTabs');
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{ name: 'MainTabs' }],
+        }),
+      );
+      // 진짜로 나갈 건지 사용자에게 물어보는 기능 추가 예정 (나가기, 임시저장하기)
+      // 나가기 누르면 전역 상태 값 다 초기화 시키기
     }
   };
 
@@ -79,13 +93,7 @@ const ExhibitScreen: React.FC = () => {
       case 1:
         return <ArtworkSelect />;
       case 2:
-        return (
-          <ThemeSetting
-            selectedThemes={selectedThemes}
-            setSelectedThemes={setSelectedThemes}
-            goToNext={goToNext}
-          />
-        );
+        return <ThemeSetting />;
       case 3:
         return <ArtworkInfoSetting />;
       case 4:
