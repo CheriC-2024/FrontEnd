fb7ab6f7039277e57426172cacf91fd0eae8215c
Minji Kim <minjikim2000@hotmail.com>
2024-06-01 16:21:18 +0900

    feat: add theme-words save function by using global state #9
    현재는 React Context을 사용했지만, api 연결을 위해 Redux를 적용할 예정

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -6,6 +6,7 @@ import ExhibitScreen from '../screens/Exhibit/ExhibitScreen';
 import AIRecommendLoading from '../screens/AIRecommendation/AIRecommendLoading';
 import AIRecommend from '../screens/AIRecommendation/AIRecommend';
 import { ProgressBarProvider } from '../components/ProgressBarContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
 
 type RootStackParamList = {
   MainTabs: undefined;
@@ -19,30 +20,32 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <ProgressBarProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="MainTabs">
-          <Stack.Screen
-            name="MainTabs"
-            component={MainTabs}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Exhibit"
-            component={ExhibitScreen}
-            options={{ headerTitle: '' }}
-          />
-          <Stack.Screen
-            name="AIRecommendLoading"
-            component={AIRecommendLoading}
-            options={{ headerTitle: 'AI 테마 추천' }}
-          />
-          <Stack.Screen
-            name="AIRecommend"
-            component={AIRecommend}
-            options={{ headerTitle: 'AI 테마 선택' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="MainTabs">
+            <Stack.Screen
+              name="MainTabs"
+              component={MainTabs}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Exhibit"
+              component={ExhibitScreen}
+              options={{ headerTitle: '' }}
+            />
+            <Stack.Screen
+              name="AIRecommendLoading"
+              component={AIRecommendLoading}
+              options={{ headerTitle: 'AI 테마 추천' }}
+            />
+            <Stack.Screen
+              name="AIRecommend"
+              component={AIRecommend}
+              options={{ headerTitle: 'AI 테마 선택' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </ProgressBarProvider>
   );
 };
