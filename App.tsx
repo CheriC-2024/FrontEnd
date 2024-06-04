8ba8f94421113b8ebd26fcafe61ac49847088fef
Minji Kim <minjikim2000@hotmail.com>
2024-06-04 21:45:44 +0900

    feat: add global fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import GlobalStyle from './src/styles/globalStyles';
 import AppNavigator from './src/navigations/AppNavigator';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <>
+      <GlobalStyle />
+      <AppNavigator />
+    </>
+  );
 };
 
 export default App;
