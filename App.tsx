3edab73f3d7deb6f0f7c2df8ea36ab05d3c2ea75
Minji Kim <minjikim2000@hotmail.com>
2024-06-05 12:17:22 +0900

    feat: add fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import GlobalStyle from './src/styles/globalStyles';
+import React, { useEffect, useState } from 'react';
 import AppNavigator from './src/navigations/AppNavigator';
+import * as Font from 'expo-font';
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <AppNavigator />
-    </>
-  );
+  //폰트 적용
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        Thin: require('./src/assets/fonts/Pretendard/Pretendard-Thin.otf'),
+        ExtraLight: require('./src/assets/fonts/Pretendard/Pretendard-ExtraLight.otf'),
+        Light: require('./src/assets/fonts/Pretendard/Pretendard-Light.otf'),
+        Regular: require('./src/assets/fonts/Pretendard/Pretendard-Regular.otf'),
+        Medium: require('./src/assets/fonts/Pretendard/Pretendard-Medium.otf'),
+        SemiBold: require('./src/assets/fonts/Pretendard/Pretendard-SemiBold.otf'),
+        Bold: require('./src/assets/fonts/Pretendard/Pretendard-Bold.otf'),
+        ExtraBold: require('./src/assets/fonts/Pretendard/Pretendard-ExtraBold.otf'),
+        Black: require('./src/assets/fonts/Pretendard/Pretendard-Black.otf'),
+      });
+      setFontsLoaded(true);
+    };
+    loadFonts();
+  }, []);
+
+  return <AppNavigator />;
 };
 
 export default App;
