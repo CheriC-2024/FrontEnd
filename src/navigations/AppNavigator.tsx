620984b3a2ba6c4deb255e461011ed2bf6954eef
Minji Kim <minjikim2000@hotmail.com>
2024-06-07 14:40:38 +0900

    feat: update RootStackParamList for dynamic screen navigation #13

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -7,46 +7,107 @@ import AIRecommendLoading from '../screens/AIRecommendation/AIRecommendLoading';
 import AIRecommend from '../screens/AIRecommendation/AIRecommend';
 import { ProgressBarProvider } from '../components/ProgressBarContext';
 import { ThemeProvider } from '../contexts/ThemeContext';
+import AIRecommendDescription from '../screens/AIRecommendation/AIRecommendDescription';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import ArtworkList from '../screens/Exhibit/ArtworkList';
+import ArtworkDetail from '../screens/Exhibit/ArtworkDetail';
+import { GlobalStateProvider } from '../contexts/GlobalStateContext';
 
-type RootStackParamList = {
+export type RootStackParamList = {
   MainTabs: undefined;
   Exhibit: { step: number; selectedThemes?: string[] };
-  AIRecommendLoading: undefined;
-  AIRecommend: undefined;
+  AIRecommendLoading: { source: string };
+  AIRecommend: { source: string };
+  AIRecommendDescription: { source: string };
+  ArtworkList: undefined;
+  ArtworkDetail: {
+    isCollectorOnly: boolean;
+    imageUrl: any;
+    title: string;
+  };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
   return (
-    <ProgressBarProvider>
-      <ThemeProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="MainTabs">
-            <Stack.Screen
-              name="MainTabs"
-              component={MainTabs}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Exhibit"
-              component={ExhibitScreen}
-              options={{ headerTitle: '' }}
-            />
-            <Stack.Screen
-              name="AIRecommendLoading"
-              component={AIRecommendLoading}
-              options={{ headerTitle: 'AI 테마 추천' }}
-            />
-            <Stack.Screen
-              name="AIRecommend"
-              component={AIRecommend}
-              options={{ headerTitle: 'AI 테마 선택' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ThemeProvider>
-    </ProgressBarProvider>
+    <GlobalStateProvider>
+      <ProgressBarProvider>
+        <ThemeProvider>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="MainTabs"
+              screenOptions={{
+                headerTitleStyle: {
+                  fontFamily: 'Bold', // 원하는 폰트 패밀리로 변경
+                  fontSize: 16, // 원하는 폰트 크기로 변경
+                  color: '#120000', // 원하는 폰트 색상으로 변경
+                },
+              }}
+            >
+              <Stack.Screen
+                name="MainTabs"
+                component={MainTabs}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Exhibit"
+                component={ExhibitScreen}
+                options={{ headerTitle: '' }}
+              />
+              <Stack.Screen
+                name="AIRecommendLoading"
+                component={AIRecommendLoading}
+                options={{ headerTitle: 'AI 추천' }}
+              />
+              <Stack.Screen
+                name="AIRecommend"
+                component={AIRecommend}
+                options={{
+                  headerTitle: 'AI 테마 선택',
+                  headerLeft: () => null,
+                  headerBackVisible: false,
+                }}
+              />
+              <Stack.Screen
+                name="AIRecommendDescription"
+                component={AIRecommendDescription}
+                options={{
+                  headerTitle: 'AI 전시명',
+                  headerLeft: () => null,
+                  headerBackVisible: false,
+                }}
+              />
+              <Stack.Screen
+                name="ArtworkList"
+                component={ArtworkList}
+                options={({ navigation }) => ({
+                  headerTitle: '전시로 올려질 작품',
+                  headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                      <Icon name="arrow-back" size={24} color="#000" />
+                    </TouchableOpacity>
+                  ),
+                })}
+              />
+              <Stack.Screen
+                name="ArtworkDetail"
+                component={ArtworkDetail}
+                options={({ navigation }) => ({
+                  headerTitle: '작품의 상세 정보',
+                  headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                      <Icon name="arrow-back" size={24} color="#000" />
+                    </TouchableOpacity>
+                  ),
+                })}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </ThemeProvider>
+      </ProgressBarProvider>
+    </GlobalStateProvider>
   );
 };
 
