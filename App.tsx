b31939bf9903b51172d5986ff5cf47a47a107e4b
Minji Kim <minjikim2000@hotmail.com>
2024-06-07 21:35:03 +0900

    feat: modify for drag-and-drop function

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import AppNavigator from './src/navigations/AppNavigator';
 import * as Font from 'expo-font';
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
-  //폰트 적용
+  // 폰트 적용
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   useEffect(() => {
@@ -24,7 +27,15 @@ const App = () => {
     loadFonts();
   }, []);
 
-  return <AppNavigator />;
+  if (!fontsLoaded) {
+    return null; // 폰트가 로드되지 않은 경우 null을 반환하여 아무것도 렌더링하지 않음
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
+  );
 };
 
 export default App;
