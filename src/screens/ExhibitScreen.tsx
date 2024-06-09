492b2aa687ef91b9aa7c0b47b3e499b403089cf9
Minji Kim <minjikim2000@hotmail.com>
2024-06-10 05:42:13 +0900

    refactor: modify ExhibitScreen file location #19

--- src/screens/ExhibitScreen.tsx
+++ src/screens/ExhibitScreen.tsx
@@ -9,16 +9,18 @@ import {
 } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/Ionicons'; // 아이콘 라이브러리 추가
-import CollectionSelect from './CollectionSelect';
-import ArtworkSelect from './ArtworkSelect';
-import ThemeSetting from './ThemeSetting';
-import ArtworkInfoSetting from './ArtworkInfoSetting';
-import DescriptionSetting from './DescriptionSetting';
-import CoverSetting from './CoverSetting';
-import FinishSetting from './FinishSetting';
-import { useProgressBar } from '../../components/ProgressBarContext';
-import { RootStackParamList } from '../../navigations/AppNavigator';
-import CustomModal from '../../components/Modal'; // 모달 컴포넌트 임포트
+import CollectionSelect from './Exhibit/CollectionSelect';
+import ArtworkSelect from './Exhibit/ArtworkSelect';
+import ThemeSetting from './Exhibit/ThemeSetting';
+import ArtworkInfoSetting from './Exhibit/ArtworkInfoSetting';
+import DescriptionSetting from './Exhibit/DescriptionSetting';
+import CoverSetting from './Exhibit/CoverSetting';
+import FinishSetting from './Exhibit/FinishSetting';
+import { useProgressBar } from '../components/ProgressBarContext';
+import { RootStackParamList } from '../navigations/AppNavigator';
+import CustomModal from '../components/Modal'; // 모달 컴포넌트 임포트
+import ArtworkSelectModal from '../components/Modals/ArtworkSelectModal'; // 추가된 모달 컴포넌트 임포트
+import { useGlobalState } from '../contexts/GlobalStateContext'; // 상태 불러오기
 
 type ExhibitScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -34,10 +36,22 @@ const ExhibitScreen: React.FC = () => {
   const navigation = useNavigation<ExhibitScreenNavigationProp>();
   const route = useRoute<ExhibitScreenRouteProp>();
   const { setStep } = useProgressBar();
+  const {
+    collections,
+    selectedCollections,
+    userCherries,
+    selectedArtworks,
+    setSelectedArtworks,
+  } = useGlobalState(); // 상태 추가
   const [step, setLocalStep] = useState(route.params?.step || 0);
   const [isDescriptionValid, setIsDescriptionValid] = useState(false);
   const [isCollectionSelected, setIsCollectionSelected] = useState(false); // 선택된 컬렉션의 상태
   const [modalVisible, setModalVisible] = useState(false);
+  const [isArtworkModalVisible, setIsArtworkModalVisible] = useState(false); // 추가된 모달 상태
+  const [cherryDetails, setCherryDetails] = useState({
+    totalRequiredCherries: 0,
+    cherryCount: 0,
+  });
 
   useEffect(() => {
     if (route.params?.step !== undefined) {
@@ -76,6 +90,7 @@ const ExhibitScreen: React.FC = () => {
           onPress={step === 6 ? showConfirmModal : goToNext} // FinishSetting 모달
           disabled={
             (step === 0 && !isCollectionSelected) ||
+            (step === 1 && selectedArtworks.length === 0) || // 추가된 조건
             (step === 4 && !isDescriptionValid)
           }
         >
@@ -84,6 +99,7 @@ const ExhibitScreen: React.FC = () => {
               marginRight: 16,
               color:
                 (step === 0 && !isCollectionSelected) ||
+                (step === 1 && selectedArtworks.length === 0) || // 추가된 조건
                 (step === 4 && !isDescriptionValid)
                   ? '#ccc'
                   : '#120000',
@@ -96,7 +112,13 @@ const ExhibitScreen: React.FC = () => {
         </TouchableOpacity>
       ),
     });
-  }, [navigation, step, isDescriptionValid, isCollectionSelected]);
+  }, [
+    navigation,
+    step,
+    isDescriptionValid,
+    isCollectionSelected,
+    selectedArtworks,
+  ]);
 
   const showConfirmModal = () => {
     setModalVisible(true);
@@ -112,6 +134,37 @@ const ExhibitScreen: React.FC = () => {
   };
 
   const goToNext = () => {
+    if (step === 1) {
+      const newCherryDetails = selectedArtworks.reduce(
+        (acc, artworkId) => {
+          const artwork = collections
+            .flatMap((collection) => collection.artworks)
+            .find((art) => art.id === artworkId);
+          if (artwork && artwork.cherry && artwork.cherry > 0) {
+            return {
+              totalRequiredCherries: acc.totalRequiredCherries + artwork.cherry,
+              cherryCount: acc.cherryCount + 1,
+            };
+          }
+          return acc;
+        },
+        { totalRequiredCherries: 0, cherryCount: 0 },
+      );
+
+      setCherryDetails(newCherryDetails);
+
+      if (
+        newCherryDetails.totalRequiredCherries > userCherries &&
+        newCherryDetails.totalRequiredCherries > 0
+      ) {
+        setIsArtworkModalVisible(true);
+        return;
+      } else if (newCherryDetails.totalRequiredCherries > 0) {
+        setIsArtworkModalVisible(true);
+        return;
+      }
+    }
+
     if (step < 6) {
       setLocalStep(step + 1);
       setStep(step + 1);
@@ -145,7 +198,7 @@ const ExhibitScreen: React.FC = () => {
           />
         );
       case 1:
-        return <ArtworkSelect />;
+        return <ArtworkSelect onSelectionChange={setSelectedArtworks} />;
       case 2:
         return <ThemeSetting />;
       case 3:
@@ -187,6 +240,24 @@ const ExhibitScreen: React.FC = () => {
         }
         confirmButtonText="확인했습니다"
       />
+      {isArtworkModalVisible && cherryDetails.totalRequiredCherries > 0 && (
+        <ArtworkSelectModal
+          visible={isArtworkModalVisible}
+          requiredCherries={cherryDetails.totalRequiredCherries}
+          cherryCount={cherryDetails.cherryCount} // 추가된 체리 카운트
+          userCherries={userCherries}
+          onClose={() => setIsArtworkModalVisible(false)}
+          onBuyCherries={() => {
+            // Handle buying cherries
+            setIsArtworkModalVisible(false);
+          }}
+          onConfirm={() => {
+            setLocalStep(2);
+            setStep(2);
+            setIsArtworkModalVisible(false);
+          }} // step 2로 이동하는 콜백 함수
+        />
+      )}
     </Container>
   );
 };
