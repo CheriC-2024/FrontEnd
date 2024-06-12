022b43fb24ed35e06fee14035a3f38e0ea1b774a
Minji Kim <minjikim2000@hotmail.com>
2024-06-12 21:18:13 +0900

    UI: modify modal style to final design #25

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { Modal, Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import Animated, { Easing } from 'react-native-reanimated';
+import {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 interface CustomModalProps {
@@ -8,6 +15,7 @@ interface CustomModalProps {
   title: string;
   body: string | React.ReactNode;
   confirmButtonText: string;
+  cancelButtonText: string;
   onConfirm: () => void;
 }
 
@@ -17,69 +25,131 @@ const CustomModal: React.FC<CustomModalProps> = ({
   title,
   body,
   confirmButtonText,
+  cancelButtonText,
   onConfirm,
 }) => {
+  const scale = useSharedValue(0);
+
+  useEffect(() => {
+    if (visible) {
+      scale.value = withTiming(1, {
+        duration: 300,
+        easing: Easing.out(Easing.exp),
+      });
+    } else {
+      scale.value = withTiming(0, {
+        duration: 300,
+        easing: Easing.out(Easing.exp),
+      });
+    }
+  }, [visible]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
   return (
-    <Modal transparent visible={visible} animationType="slide">
-      <View style={styles.overlay}>
-        <View style={styles.modalContainer}>
-          <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-            <Icon name="close" size={24} color="#000" />
-          </TouchableOpacity>
-          <Text style={styles.title}>{title}</Text>
-          <View style={styles.body}>
-            {typeof body === 'string' ? <Text>{body}</Text> : body}
-          </View>
-          <TouchableOpacity onPress={onConfirm} style={styles.confirmButton}>
-            <Text style={styles.confirmButtonText}>{confirmButtonText}</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <Modal transparent visible={visible} animationType="none">
+      <CenteredView>
+        <Animated.View style={[animatedStyle]}>
+          <ModalContainer>
+            <CloseButton onPress={onClose}>
+              <Icon name="close" size={24} color="#000" />
+            </CloseButton>
+            <Title>{title}</Title>
+            <Body>
+              {typeof body === 'string' ? <BodyText>{body}</BodyText> : body}
+            </Body>
+            <ButtonContainer>
+              <CancelButton onPress={onClose}>
+                <CancelButtonText>{cancelButtonText}</CancelButtonText>
+              </CancelButton>
+              <ConfirmButton onPress={onConfirm}>
+                <ConfirmButtonText>{confirmButtonText}</ConfirmButtonText>
+              </ConfirmButton>
+            </ButtonContainer>
+          </ModalContainer>
+        </Animated.View>
+      </CenteredView>
     </Modal>
   );
 };
 
-const styles = StyleSheet.create({
-  overlay: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContainer: {
-    width: '80%',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
-    position: 'relative',
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  body: {
-    marginBottom: 20,
-    alignItems: 'center',
-  },
-  confirmButton: {
-    backgroundColor: '#F3F3F3',
-    padding: 10,
-    borderRadius: 5,
-    width: '100%',
-    alignItems: 'center',
-  },
-  confirmButtonText: {
-    color: '#000',
-    fontSize: 16,
-  },
-});
+const CenteredView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px; /* 모달 위치 */
+`;
+
+const ModalContainer = styled.View`
+  flex-direction: column;
+  margin: 15px;
+  padding: 60px 35px;
+  background-color: white;
+  border-radius: 20px;
+  align-items: start;
+  elevation: 5;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+const Title = styled.Text`
+  font-family: 'Bold';
+  font-size: 22px;
+  text-align: left;
+  margin-bottom: 15px;
+`;
+
+const Body = styled.View`
+  margin-bottom: 25px;
+  align-items: start;
+`;
+
+const BodyText = styled.Text`
+  text-align: left;
+  font-family: 'Regular';
+  font-size: 16px;
+  line-height: 22px;
+  color: #b0abab;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+const CancelButton = styled.TouchableOpacity`
+  background-color: #fff;
+  border-radius: 20px;
+  padding: 12px 27px;
+  border: 1.5px solid #f7f5f5;
+`;
+
+const ConfirmButton = styled.TouchableOpacity`
+  background-color: #f7f5f5;
+  border-radius: 20px;
+  padding: 12px 27px;
+`;
+
+const CancelButtonText = styled.Text`
+  font-family: 'Regular';
+  font-size: 16px;
+  color: #b0abab;
+  text-align: center;
+`;
+
+const ConfirmButtonText = styled.Text`
+  font-family: 'Regular';
+  font-size: 16px;
+  color: #120000;
+`;
 
 export default CustomModal;
