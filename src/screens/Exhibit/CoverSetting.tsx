1e8ba5a96ab0109d41509a9268885121b00e7c6b
Minji Kim <minjikim2000@hotmail.com>
2024-06-13 11:10:44 +0900

    UI: add with gradient covers #27

--- src/screens/Exhibit/CoverSetting.tsx
+++ src/screens/Exhibit/CoverSetting.tsx
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styled from 'styled-components/native';
 import ProgressBarComponent from '../../components/ProgressBar';
 import { useProgressBar } from '../../components/ProgressBarContext';
 import { LinearGradient } from 'expo-linear-gradient';
+import RadialGradientComponent from '../../components/Gradients/RadialGradientComponent';
+import AngularGradientComponent from '../../components/Gradients/AngularGradientComponent';
+import DiamondGradientComponent from '../../components/Gradients/DiamondGradientComponent';
+import * as ImagePicker from 'expo-image-picker';
 
 const CoverSetting: React.FC = () => {
-  const { step, setStep } = useProgressBar();
-  const [selectedCover, setSelectedCover] = useState([
-    '#FFDAB9',
-    '#FF6347',
-    '#FFA07A',
-    '#FF4500',
-  ]);
-  const [isReversed, setIsReversed] = useState(false);
-  const [selectedPalette, setSelectedPalette] = useState(0);
-
   const colorPalettes = [
     ['#FFE4E1', '#FFB6C1', '#FF69B4', '#FF1493'], // Pink palette
     ['#98FB98', '#00FA9A', '#00FF7F', '#3CB371'], // Green palette
@@ -24,11 +18,19 @@ const CoverSetting: React.FC = () => {
     ['#FFFFE0', '#FFD700', '#FFA500', '#FF8C00'], // Yellow-Orange palette
   ];
 
+  const { step, setStep } = useProgressBar();
+  const [selectedCover, setSelectedCover] = useState(colorPalettes[0]);
+  const [isReversed, setIsReversed] = useState(false);
+  const [selectedPalette, setSelectedPalette] = useState(0);
+  const [selectedGradient, setSelectedGradient] = useState<number | null>(null);
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [coverType, setCoverType] = useState<'gradient' | 'solid'>('gradient');
+
   useEffect(() => {
     setStep(5); // Set progress bar to step 6 (index 5)
   }, [step]);
 
-  const handleCoverSelect = (index) => {
+  const handleCoverSelect = (index: number) => {
     setSelectedPalette(index);
     setSelectedCover(colorPalettes[index]);
   };
@@ -37,91 +39,164 @@ const CoverSetting: React.FC = () => {
     setIsReversed(!isReversed);
   };
 
+  const handleGradientSelect = (index: number) => {
+    setSelectedGradient(selectedGradient === index ? null : index);
+  };
+
+  const handleImageUpload = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1], // Set aspect ratio to 1:1
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setUploadedImage(result.assets[0].uri);
+    }
+  };
+
+  const handleImageRemove = () => {
+    setUploadedImage(null);
+  };
+
+  const handleCoverTypeChange = (type: 'gradient' | 'solid') => {
+    setCoverType(type);
+    setSelectedGradient(null); // Reset selected gradient
+  };
+
   return (
     <Container>
       <ProgressBarComponent totalSteps={7} />
-      <TitleContainer>
-        <TitleIcon source={require('../../assets/images/character.png')} />
-        <TitleText>
-          <Title>전시 커버 설정하기</Title>
-          <Subtitle>전시와 어울리는 커버를 선택해주세요!</Subtitle>
-        </TitleText>
-      </TitleContainer>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <TitleContainer>
+          <TitleIcon source={require('../../assets/images/character.png')} />
+          <TitleText>
+            <Title>전시 커버 설정하기</Title>
+            <Subtitle>전시와 어울리는 커버를 선택해주세요!</Subtitle>
+          </TitleText>
+        </TitleContainer>
 
-      <CoverTitle>체리시가 제안하는 커버</CoverTitle>
-      <CoverTextContainer>
-        <CoverSubtitle>
-          컬렉터님의 전시 작품 컬러를 바탕으로 만들었어요 :)
-        </CoverSubtitle>
-        <TouchableOpacity onPress={handleReverseGradient}>
-          <Icon name="sync-outline" size={22} color="#120000" />
-        </TouchableOpacity>
-      </CoverTextContainer>
+        <CoverTitle>체리시가 제안하는 커버</CoverTitle>
+        <CoverTextContainer>
+          <CoverSubtitle>
+            컬렉터님의 전시 작품 컬러를 바탕으로 만들었어요 :)
+          </CoverSubtitle>
+          <TouchableOpacity>
+            <Icon name="sync-outline" size={22} color="#120000" />
+          </TouchableOpacity>
+        </CoverTextContainer>
 
-      <PaletteContainer>
-        {colorPalettes.map((palette, index) => (
-          <PaletteButton
-            key={index}
-            onPress={() => handleCoverSelect(index)}
-            selected={selectedPalette === index}
-          >
-            {palette.map((color, colorIndex) => (
-              <PaletteColor key={colorIndex} color={color} />
-            ))}
-          </PaletteButton>
-        ))}
-      </PaletteContainer>
+        <PaletteContainer>
+          {colorPalettes.map((palette, index) => (
+            <PaletteButton
+              key={index}
+              onPress={() => handleCoverSelect(index)}
+              selected={selectedPalette === index}
+            >
+              {palette.map((color, colorIndex) => (
+                <PaletteColor key={colorIndex} color={color} />
+              ))}
+            </PaletteButton>
+          ))}
+        </PaletteContainer>
 
-      <CoverSection>
         <CoverTypeButtonContainer>
-          <CoverTypeButton selected>그라디언트 커버</CoverTypeButton>
-          <CoverTypeButton>단색 커버</CoverTypeButton>
+          <View style={{ flexDirection: 'row', flex: 1 }}>
+            <CoverTypeButton
+              selected={coverType === 'gradient'}
+              onPress={() => handleCoverTypeChange('gradient')}
+            >
+              <CoverTypeButtonText selected={coverType === 'gradient'}>
+                그라디언트 커버
+              </CoverTypeButtonText>
+            </CoverTypeButton>
+            <CoverTypeButton
+              selected={coverType === 'solid'}
+              onPress={() => handleCoverTypeChange('solid')}
+            >
+              <CoverTypeButtonText selected={coverType === 'solid'}>
+                단색 커버
+              </CoverTypeButtonText>
+            </CoverTypeButton>
+          </View>
+          {coverType === 'gradient' && (
+            <TouchableOpacity onPress={handleReverseGradient}>
+              <Icon name="swap-horizontal-outline" size={22} color="#120000" />
+            </TouchableOpacity>
+          )}
         </CoverTypeButtonContainer>
-        <CoverGrid>
-          <CoverOption>
-            <LinearGradient
-              colors={isReversed ? [...selectedCover].reverse() : selectedCover}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 1 }}
-              style={styles.gradient}
-            />
-            <SelectedIndicator />
-          </CoverOption>
-          <CoverOption>
-            <LinearGradient
-              colors={isReversed ? [...selectedCover].reverse() : selectedCover}
-              start={{ x: 1, y: 1 }}
-              end={{ x: 0, y: 0 }}
-              style={styles.gradient2}
-            />
-            <SelectedIndicator />
-          </CoverOption>
-          <CoverOption>
-            <LinearGradient
-              colors={isReversed ? [...selectedCover].reverse() : selectedCover}
-              start={{ x: 0, y: 1 }}
-              end={{ x: 1, y: 0 }}
-              style={styles.gradient3}
-            />
-            <SelectedIndicator />
-          </CoverOption>
-          <CoverOption>
-            <LinearGradient
-              colors={isReversed ? [...selectedCover].reverse() : selectedCover}
-              start={{ x: 1, y: 0 }}
-              end={{ x: 0, y: 1 }}
-              style={styles.gradient4}
-            />
-            <SelectedIndicator />
-          </CoverOption>
-        </CoverGrid>
-      </CoverSection>
 
-      <UploadSection>
-        <UploadButton onPress={() => console.log('Add cover image')}>
-          <UploadButtonText>직접 커버 이미지 추가하기</UploadButtonText>
-        </UploadButton>
-      </UploadSection>
+        {coverType === 'gradient' ? (
+          <CoverSection>
+            <CoverGrid>
+              {[
+                LinearGradient,
+                RadialGradientComponent,
+                AngularGradientComponent,
+                DiamondGradientComponent,
+              ].map((GradientComponent, index) => (
+                <CoverOption
+                  key={index}
+                  onPress={() => handleGradientSelect(index)}
+                >
+                  <GradientComponent
+                    colors={
+                      isReversed ? [...selectedCover].reverse() : selectedCover
+                    }
+                    style={[
+                      styles.gradient,
+                      selectedGradient === index && styles.selectedGradient,
+                    ]}
+                  />
+                  {selectedGradient === index && (
+                    <SelectedOverlay>
+                      <Icon name="checkmark" size={32} color="#fff" />
+                    </SelectedOverlay>
+                  )}
+                </CoverOption>
+              ))}
+            </CoverGrid>
+          </CoverSection>
+        ) : (
+          <CoverSection>
+            <CoverGrid>
+              {colorPalettes[selectedPalette].map((color, index) => (
+                <CoverOption
+                  key={index}
+                  onPress={() => handleGradientSelect(index)}
+                >
+                  <View style={[styles.gradient, { backgroundColor: color }]}>
+                    {selectedGradient === index && (
+                      <SelectedOverlay>
+                        <Icon name="checkmark" size={32} color="#fff" />
+                      </SelectedOverlay>
+                    )}
+                  </View>
+                </CoverOption>
+              ))}
+            </CoverGrid>
+          </CoverSection>
+        )}
+
+        <UploadSection>
+          <UploadTitle>직접 커버 이미지 추가하기</UploadTitle>
+          <UploadButton onPress={handleImageUpload}>
+            <UploadPlaceholder>
+              {uploadedImage ? (
+                <>
+                  <UploadedImage source={{ uri: uploadedImage }} />
+                  <DeleteIcon onPress={handleImageRemove}>
+                    <Icon name="close" size={24} color="#fff" />
+                  </DeleteIcon>
+                </>
+              ) : (
+                <Icon name="add" size={40} color="#999" />
+              )}
+            </UploadPlaceholder>
+          </UploadButton>
+        </UploadSection>
+      </ScrollView>
     </Container>
   );
 };
@@ -182,109 +257,129 @@ const CoverSubtitle = styled.Text`
   letter-spacing: 0.5px;
 `;
 
+const PaletteContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+  flex-wrap: wrap;
+`;
+
+const PaletteButton = styled.TouchableOpacity<{ selected?: boolean }>`
+  flex-direction: row;
+  width: 80px;
+  height: 40px;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-right: 10px;
+  border-width: 2px;
+  border-color: ${(props) => (props.selected ? '#000' : 'transparent')};
+  border-style: ${(props) => (props.selected ? 'dashed' : 'solid')};
+`;
+
+const PaletteColor = styled.View<{ color: string }>`
+  flex: 1;
+  background-color: ${(props) => props.color};
+`;
+
 const CoverSection = styled.View`
   margin-bottom: 16px;
 `;
 
 const CoverTypeButtonContainer = styled.View`
   flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
   margin-bottom: 16px;
 `;
 
-const CoverTypeButton = styled.Text`
-  font-family: 'Regular';
-  font-size: 14px;
-  color: ${(props) => (props.selected ? '#120000' : '#999')};
-  margin-right: 16px;
-  border-bottom-width: ${(props) => (props.selected ? '2px' : '0px')};
-  border-bottom-color: #120000;
+const CoverTypeButton = styled.TouchableOpacity<{ selected?: boolean }>`
+  padding: 4px 10px;
+  border-radius: 20px;
+  margin-right: 8px;
+  border: 1px ${(props) => (props.selected ? 'transparent' : '#F7F5F5')};
+  background-color: ${(props) => (props.selected ? '#120000' : '#ffffff')};
 `;
 
-const CoverOption = styled.View`
-  margin-right: 10px;
+const CoverTypeButtonText = styled.Text<{ selected?: boolean }>`
+  font-family: 'Regular';
+  font-size: 12px;
+  color: ${(props) => (props.selected ? '#fff' : '#120000')};
+`;
+
+const CoverOption = styled.TouchableOpacity`
   position: relative;
+  margin-bottom: 20px;
 `;
 
 const CoverGrid = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
-const SelectedIndicator = styled.View`
+const SelectedOverlay = styled.View`
   position: absolute;
   top: 0;
+  left: 0;
   right: 0;
-  width: 20px;
-  height: 20px;
-  background-color: #120000;
+  bottom: 0;
   border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.5);
   justify-content: center;
   align-items: center;
 `;
 
 const UploadSection = styled.View`
-  margin-top: 16px;
-  align-items: center;
+  padding-left: 20px;
+  align-items: left;
+  margin-bottom: 30px;
+`;
+
+const UploadTitle = styled.Text`
+  font-family: 'Bold';
+  font-size: 14px;
+  color: #120000;
+  margin-bottom: 10px;
 `;
 
 const UploadButton = styled.TouchableOpacity`
-  padding: 10px 20px;
-  background-color: #120000;
-  border-radius: 5px;
-`;
-
-const UploadButtonText = styled.Text`
-  font-family: 'Bold';
-  font-size: 14px;
-  color: #fff;
-`;
-
-const PaletteContainer = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 16px;
-`;
-
-const PaletteButton = styled.TouchableOpacity`
-  flex-direction: row;
-  width: 100px;
-  height: 40px;
+  width: 155px;
+  height: 155px;
+  background-color: #f7f5f5;
   border-radius: 10px;
+  justify-content: center;
+  align-items: center;
   overflow: hidden;
-  margin-right: 10px;
-  border-width: ${(props) => (props.selected ? '2px' : '0px')};
-  border-color: ${(props) => (props.selected ? '#000' : 'transparent')};
 `;
 
-const PaletteColor = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.color};
+const UploadPlaceholder = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UploadedImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const DeleteIcon = styled.TouchableOpacity`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 15px;
+  padding: 5px;
 `;
 
 const styles = {
   gradient: {
-    width: 150,
-    height: 150,
+    width: 155,
+    height: 155,
     borderRadius: 10,
-  },
-  gradient2: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-    transform: [{ rotate: '45deg' }],
-  },
-  gradient3: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-    transform: [{ rotate: '90deg' }],
-  },
-  gradient4: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-    transform: [{ rotate: '135deg' }],
   },
 };
 
