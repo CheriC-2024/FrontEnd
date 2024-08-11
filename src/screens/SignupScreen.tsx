853ca433fcbabade01388483697d7c4a655043f6
Minji Kim <minjikim2000@hotmail.com>
2024-08-12 03:33:57 +0900

    UI: add image to interest button #31

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,102 +1,15 @@
 import React, { useState } from 'react';
-import styled from 'styled-components/native';
-import { ScrollView, TextInput, View } from 'react-native';
-import { H1, Subtitle2, Body1 } from '../components/Typography';
+import styled, { useTheme } from 'styled-components/native';
+import { ScrollView, TextInput, View, ImageBackground } from 'react-native';
+import { H1, H5, Subtitle2, Body1, ButtonText } from '../components/Typography';
 import InfoBlock from '../components/InfoBlock';
-import { Button, ButtonText } from '../components/Button';
+import TitleSubtitle from '../components/TitleSubtitle';
+import { Btn, BtnText } from '../components/Button';
 import ToastMessage from '../components/ToastMessage';
 import { useToastMessage } from '../hooks/useToastMessage';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.colors.white};
-  padding: ${(props) => props.theme.spacing.s6};
-`;
-
-const Title = styled(H1)`
-  color: ${(props) => props.theme.colors.redBlack};
-  margin-bottom: ${(props) => props.theme.spacing.s2};
-`;
-
-const SubtitleText = styled(Subtitle2)`
-  color: ${(props) => props.theme.colors.grey_8};
-  margin-bottom: ${(props) => props.theme.spacing.s4};
-`;
-
-const InputLabel = styled(Subtitle2)`
-  color: ${(props) => props.theme.colors.redBlack};
-  margin-bottom: ${(props) => props.theme.spacing.s2};
-`;
-
-const InputField = styled(TextInput)`
-  border: 1px solid ${(props) => props.theme.colors.grey_6};
-  border-radius: ${(props) => props.theme.radius.s};
-  padding: ${(props) => props.theme.spacing.s2};
-  margin-bottom: ${(props) => props.theme.spacing.s4};
-  font-family: ${(props) => props.theme.fonts.regular};
-`;
-
-const InterestSection = styled.View`
-  margin-bottom: ${(props) => props.theme.spacing.s6};
-`;
-
-const InterestButton = styled.TouchableOpacity<{ selected: boolean }>`
-  width: 80px;
-  height: 80px;
-  background-color: ${(props) =>
-    props.selected ? props.theme.colors.cherryRed_10 : '#CCCCCC'};
-  border-radius: 40px;
-  align-items: center;
-  justify-content: center;
-  margin: ${(props) => props.theme.spacing.s1};
-`;
-
-const InterestText = styled(Body1)<{ selected: boolean }>`
-  color: ${(props) =>
-    props.selected ? props.theme.colors.white : props.theme.colors.redBlack};
-  font-family: ${(props) =>
-    props.selected ? props.theme.fonts.bold : props.theme.fonts.regular};
-  text-align: center;
-`;
-
-const RadioSection = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: ${(props) => props.theme.spacing.s6};
-`;
-
-const RadioButton = styled.TouchableOpacity<{ selected: boolean }>`
-  flex-direction: row;
-  align-items: center;
-  padding: ${(props) => props.theme.spacing.s2};
-  border-radius: ${(props) => props.theme.radius.s};
-`;
-
-const RadioButtonCircle = styled.View<{ selected: boolean }>`
-  width: 20px;
-  height: 20px;
-  border-radius: 10px;
-  background-color: ${(props) => props.theme.colors.grey_4};
-  align-items: center;
-  justify-content: center;
-  margin-right: ${(props) => props.theme.spacing.s2};
-`;
-
-const RadioButtonInnerCircle = styled.View<{ selected: boolean }>`
-  width: 10px;
-  height: 10px;
-  border-radius: 5px;
-  background-color: ${(props) =>
-    props.selected ? props.theme.colors.cherryRed_10 : 'transparent'};
-`;
-
-const RadioLabel = styled(Body1)<{ selected: boolean }>`
-  color: ${(props) => props.theme.colors.redBlack};
-  font-family: ${(props) =>
-    props.selected ? props.theme.fonts.bold : props.theme.fonts.regular};
-`;
-
 const SignupScreen: React.FC = () => {
+  const theme = useTheme();
   const [collectingExperience, setCollectingExperience] = useState<
     string | null
   >(null);
@@ -115,29 +28,57 @@ const SignupScreen: React.FC = () => {
   };
 
   const interestOptions = [
-    '뉴미디어',
-    '동화책',
-    '드로잉',
-    '디자인',
-    '수채화',
-    '유화',
-    '판화',
-    '회화',
+    {
+      label: '뉴미디어',
+      image: require('../assets/images/SignUpPage/interest1.png'),
+    },
+    {
+      label: '동화책',
+      image: require('../assets/images/SignUpPage/interest2.png'),
+    },
+    {
+      label: '드로잉',
+      image: require('../assets/images/SignUpPage/interest3.png'),
+    },
+    {
+      label: '디자인',
+      image: require('../assets/images/SignUpPage/interest4.png'),
+    },
+    {
+      label: '수채화',
+      image: require('../assets/images/SignUpPage/interest5.png'),
+    },
+    {
+      label: '유화',
+      image: require('../assets/images/SignUpPage/interest6.png'),
+    },
+    {
+      label: '판화',
+      image: require('../assets/images/SignUpPage/interest7.png'),
+    },
+    {
+      label: '회화',
+      image: require('../assets/images/SignUpPage/interest8.png'),
+    },
   ];
 
   return (
     <ScrollView>
       <Container>
-        <Title>CheriC에 처음 오셨군요!</Title>
-        <SubtitleText>
-          컬렉터님을 알아갈 수 있게 정보를 입력해주세요
-        </SubtitleText>
+        <TitleSubtitle
+          title={<H5>CheriC에 처음 오셨군요!</H5>}
+          subtitle={
+            <ButtonText>
+              컬렉터님을 알아갈 수 있게 정보를 입력해주세요
+            </ButtonText>
+          }
+        />
 
         <InputLabel>닉네임 *</InputLabel>
         <InputField placeholder="컬렉터님의 닉네임을 작성해 주세요" />
 
         <InfoBlock
-          label="전시 설명"
+          label="자기소개"
           placeholder="컬렉터님의 자기 소개를 작성해 주세요"
           maxLength={100}
         />
@@ -145,17 +86,30 @@ const SignupScreen: React.FC = () => {
         <InputLabel>관심 분야(최대 2개) *</InputLabel>
         <InterestSection>
           <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-            {interestOptions.map((interest) => (
-              <InterestButton
-                key={interest}
-                selected={interests.includes(interest)}
-                onPress={() => toggleInterest(interest)}
-              >
-                <InterestText selected={interests.includes(interest)}>
-                  {interest}
-                </InterestText>
-              </InterestButton>
-            ))}
+            {interestOptions.map((interest, index) => {
+              const overlayColor =
+                index < 4
+                  ? theme.colors.redBlack_alpha20
+                  : theme.colors.redBlack_alpha40;
+
+              return (
+                <InterestButton
+                  key={interest.label}
+                  selected={interests.includes(interest.label)}
+                  onPress={() => toggleInterest(interest.label)}
+                >
+                  <InterestButtonImage source={interest.image}>
+                    <Overlay
+                      overlayColor={overlayColor}
+                      selected={interests.includes(interest.label)}
+                    />
+                    <InterestText selected={interests.includes(interest.label)}>
+                      {interest.label}
+                    </InterestText>
+                  </InterestButtonImage>
+                </InterestButton>
+              );
+            })}
           </View>
         </InterestSection>
 
@@ -218,5 +172,104 @@ const SignupScreen: React.FC = () => {
     </ScrollView>
   );
 };
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.white};
+  padding: ${(props) => props.theme.spacing.s6};
+`;
+
+const InputLabel = styled(Subtitle2)`
+  color: ${(props) => props.theme.colors.redBlack};
+  margin-top: ${(props) => props.theme.spacing.s7};
+  margin-left: ${(props) => props.theme.spacing.s1};
+  margin-bottom: ${(props) => props.theme.spacing.s1};
+`;
+
+const InputField = styled(TextInput)`
+  border-radius: ${(props) => props.theme.radius.m};
+  padding-left: ${(props) => props.theme.spacing.s4};
+  padding-top: ${(props) => props.theme.spacing.s2};
+  padding-bottom: ${(props) => props.theme.spacing.s2};
+  margin-bottom: ${(props) => props.theme.spacing.s7};
+  font-family: ${(props) => props.theme.fonts.regular};
+  font-size: ${({ theme }) => theme.fontSizes.body2};
+  background-color: ${(props) => props.theme.colors.grey_4};
+`;
+
+const InterestSection = styled.View`
+  margin-top: ${(props) => props.theme.spacing.s2};
+  margin-bottom: ${(props) => props.theme.spacing.s6};
+`;
+
+const InterestButton = styled.TouchableOpacity<{ selected: boolean }>`
+  width: 77px;
+  height: 77px;
+  border-radius: 39px;
+  overflow: hidden;
+  margin: 6px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InterestButtonImage = styled(ImageBackground)`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Overlay = styled.View<{ overlayColor: string; selected: boolean }>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) =>
+    props.selected ? props.theme.colors.cherryRed_alpha80 : props.overlayColor};
+  z-index: 1;
+`;
+
+const InterestText = styled(Body1)<{ selected: boolean }>`
+  color: ${(props) => props.theme.colors.white};
+  font-family: ${(props) => props.theme.fonts.bold};
+  text-align: center;
+  z-index: 2;
+`;
+
+const RadioSection = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: ${(props) => props.theme.spacing.s6};
+`;
+
+const RadioButton = styled.TouchableOpacity<{ selected: boolean }>`
+  flex-direction: row;
+  align-items: center;
+  padding: ${(props) => props.theme.spacing.s2};
+  border-radius: ${(props) => props.theme.radius.s};
+`;
+
+const RadioButtonCircle = styled.View<{ selected: boolean }>`
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.colors.grey_4};
+  align-items: center;
+  justify-content: center;
+  margin-right: ${(props) => props.theme.spacing.s2};
+`;
+
+const RadioButtonInnerCircle = styled.View<{ selected: boolean }>`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: ${(props) =>
+    props.selected ? props.theme.colors.cherryRed_10 : 'transparent'};
+`;
+
+const RadioLabel = styled(Body1)<{ selected: boolean }>`
+  color: ${(props) => props.theme.colors.redBlack};
+  font-family: ${(props) =>
+    props.selected ? props.theme.fonts.bold : props.theme.fonts.regular};
+`;
 
 export default SignupScreen;
