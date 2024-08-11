6e1c7a99ff7dcaf602010aa1f5ccf5c8bfaa8e01
Minji Kim <minjikim2000@hotmail.com>
2024-08-12 03:30:46 +0900

    refacter: modify button component name #31

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Button = styled.TouchableOpacity`
+export const Btn = styled.TouchableOpacity`
   background-color: ${({ theme }) => theme.colors.redBlack};
   padding: ${({ theme }) =>
     theme.spacing.s4}; //padding값은 디자인 시스템 수정되는데로 변경 예정
@@ -9,7 +9,7 @@ export const Button = styled.TouchableOpacity`
   justify-content: center;
 `;
 
-export const ButtonText = styled.Text`
+export const BtnText = styled.Text`
   color: ${({ theme }) => theme.colors.white};
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${({ theme }) => theme.fontSizes.body1};
