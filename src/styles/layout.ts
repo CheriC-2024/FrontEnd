103af21db57260801385f961406d26172235e5f7
Minji Kim <minjikim2000@hotmail.com>
2024-08-15 22:56:01 +0900

    feat: add layout for reusable styles #31

---
+++ src/styles/layout.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 ${({ theme }) => theme.spacing.s4};
+  background-color: #fff;
+  position: relative;
+`;
