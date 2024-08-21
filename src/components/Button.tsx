6bb7d11608ce3506a20841c653c001097bc53ffc
Minji Kim <minjikim2000@hotmail.com>
2024-08-21 09:21:50 +0900

    UI: modify button position to be fixed bottom #31

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components/native';
 
 export const Btn = styled.TouchableOpacity<{ disabled?: boolean }>`
+  position: absolute;
+  bottom: 16px;
+  left: 0;
+  right: 0;
   background-color: ${({ theme, disabled }) =>
     disabled ? theme.colors.grey_6 : theme.colors.redBlack};
   padding: ${({ theme }) => theme.spacing.s4};
   border-radius: ${({ theme }) => theme.radius.l};
   align-items: center;
   justify-content: center;
+  margin: 0 ${({ theme }) => theme.margin.m};
 `;
 
 export const BtnText = styled.Text<{ disabled?: boolean }>`
