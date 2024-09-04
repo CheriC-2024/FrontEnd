7dbfc98d37570b2e876a6b9341462f773940b2a5
Minji Kim <minjikim2000@hotmail.com>
2024-09-05 08:08:13 +0900

    refactor: modify css declaration order #31

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  position: relative;
   padding: 0 ${({ theme }) => theme.spacing.s4};
   background-color: #fff;
-  position: relative;
 `;
