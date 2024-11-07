8c9454af711e8366d3a15deef6e1e0124da0e8d7
Minji Kim <minjikim2000@hotmail.com>
2024-11-08 00:35:48 +0900

    refactor: update using slide animation hook #35

--- src/components/BottomSheets/ArtCollectingSheet.tsx
+++ src/components/BottomSheets/ArtCollectingSheet.tsx
@@ -1,65 +1,27 @@
-import React, { useEffect, useRef } from 'react';
-import {
-  Modal,
-  View,
-  TouchableWithoutFeedback,
-  Animated,
-  Easing,
-} from 'react-native';
-import { Body2, ButtonText, H4, Subtitle2 } from 'src/styles/typography';
+import React from 'react';
+import { Modal, View, TouchableWithoutFeedback, Animated } from 'react-native';
+import { Body2, ButtonText, H4 } from 'src/styles/typography';
 import styled from 'styled-components/native';
+import useSlideAnimation from 'src/hooks/useSlideAnimation';
 
 interface BottomSheetProps {
   onSelect: (mode: 'image' | 'artist' | 'both') => void;
   onClose: () => void;
-  selectedOption: 'image' | 'artist' | 'both'; // 선택된 상태를 부모로부터 전달받음
+  selectedOption: 'image' | 'artist' | 'both';
 }
 
 const ArtCollectingSheet: React.FC<BottomSheetProps> = ({
   onSelect,
   onClose,
-  selectedOption, // 부모로부터 선택된 옵션을 받음
+  selectedOption,
 }) => {
-  const slideAnim = useRef(new Animated.Value(300)).current; // 모달의 초기 위치를 설정 (화면 아래)
+  const { slideAnim, slideOut } = useSlideAnimation();
 
-  useEffect(() => {
-    // 모달이 나타날 때 애니메이션 시작
-    Animated.timing(slideAnim, {
-      toValue: 0, // 화면 위로 이동
-      duration: 300, // 애니메이션 지속 시간 (밀리초)
-      easing: Easing.out(Easing.ease), // 부드러운 애니메이션을 위한 이징 함수
-      useNativeDriver: true,
-    }).start();
-  }, []);
-
-  const animateClose = (callback?: () => void) => {
-    // 모달이 사라질 때 애니메이션 실행
-    Animated.timing(slideAnim, {
-      toValue: 330, // 다시 화면 아래로 이동
-      duration: 300,
-      easing: Easing.in(Easing.ease),
-      useNativeDriver: true,
-    }).start(() => {
-      if (callback) {
-        callback();
-      }
-    });
-  };
-
-  const handleClose = () => {
-    animateClose(() => {
-      onClose(); // 애니메이션이 끝난 후 onClose 호출
-    });
-  };
+  const handleClose = () => slideOut(onClose);
 
   const handleSelect = (mode: 'image' | 'artist' | 'both') => {
-    onSelect(mode); // 선택된 옵션을 즉시 부모에 전달
-    // 선택된 상태가 화면에 반영되도록 잠시 기다린 후 애니메이션 시작
-    setTimeout(() => {
-      animateClose(() => {
-        onClose(); // 애니메이션이 끝난 후 onClose 호출
-      });
-    }, 0); // 0 밀리초 딜레이로 다음 이벤트 루프로 넘김
+    onSelect(mode);
+    slideOut(onClose);
   };
 
   return (
@@ -80,7 +42,6 @@ const ArtCollectingSheet: React.FC<BottomSheetProps> = ({
                   </SheetDescription>
                 </View>
 
-                {/* 각 옵션 버튼의 배경색을 선택된 상태에 따라 변경 */}
                 <OptionButton
                   selected={selectedOption === 'image'}
                   onPress={() => handleSelect('image')}
@@ -89,7 +50,6 @@ const ArtCollectingSheet: React.FC<BottomSheetProps> = ({
                     이미지만 보기
                   </OptionText>
                 </OptionButton>
-
                 <OptionButton
                   selected={selectedOption === 'artist'}
                   onPress={() => handleSelect('artist')}
@@ -98,7 +58,6 @@ const ArtCollectingSheet: React.FC<BottomSheetProps> = ({
                     작가만 보기
                   </OptionText>
                 </OptionButton>
-
                 <OptionButton
                   selected={selectedOption === 'both'}
                   onPress={() => handleSelect('both')}
