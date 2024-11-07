b78970a20b293bbef860c924d5b646a57fccadc6
Minji Kim <minjikim2000@hotmail.com>
2024-11-08 00:34:04 +0900

    feat: add slide animation hook for bottom sheets #35

--- src/hooks/_index.ts
+++ src/hooks/_index.ts
@@ -1,4 +1,5 @@
 export { default as useCherryModal } from './useCherryModal';
 export { default as useCherryFinishModal } from './useCherryFinishModal';
 export { default as useImmersiveMode } from './useImmersiveMode';
+export { default as useSlideAnimation } from './useSlideAnimation';
 export { default as useToastMessage } from './useToastMessage';
---
+++ src/hooks/useSlideAnimation.ts
@@ -0,0 +1,37 @@
+import { useRef, useEffect } from 'react';
+import { Animated, Easing, Dimensions } from 'react-native';
+
+const useSlideAnimation = (initialHeightOffset = 0.5, duration = 500) => {
+  const screenHeight = Dimensions.get('window').height;
+  const slideAnim = useRef(
+    new Animated.Value(screenHeight * initialHeightOffset),
+  ).current;
+
+  const slideIn = () => {
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration,
+      easing: Easing.out(Easing.ease),
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const slideOut = (callback?: () => void) => {
+    Animated.timing(slideAnim, {
+      toValue: screenHeight * initialHeightOffset,
+      duration,
+      easing: Easing.in(Easing.ease),
+      useNativeDriver: true,
+    }).start(() => {
+      if (callback) callback();
+    });
+  };
+
+  useEffect(() => {
+    slideIn();
+  }, []);
+
+  return { slideAnim, slideOut };
+};
+
+export default useSlideAnimation;
