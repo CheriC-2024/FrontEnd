57d6a28a87b9cb040757af26a280cb89ad67bdaf
Minji Kim <minjikim2000@hotmail.com>
2024-11-08 10:38:56 +0900

    refactor: organize Login screen layout #35

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,35 +1,15 @@
 import React, { useEffect } from 'react';
 import { Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styled from 'styled-components/native';
 import {
   GoogleSignin,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
-import styled from 'styled-components/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/StackNavigator';
+import { GoogleIcon } from 'src/assets/icons/_index';
 import { H1, Subtitle2 } from '../styles/typography';
-import { useNavigation } from '@react-navigation/native';
-
-const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  background-color: ${(props) => props.theme.colors.cherryRed_10};
-`;
-
-const Title = styled(H1)`
-  color: ${(props) => props.theme.colors.white};
-  margin-top: 180px;
-  margin-bottom: 300px;
-`;
-
-const Button = styled.TouchableOpacity`
-  background-color: ${(props) => props.theme.colors.white};
-  padding: 12px 28px;
-  border-radius: ${(props) => props.theme.radius.l};
-  flex-direction: row;
-`;
-
-const ButtonText = styled(Subtitle2)``;
 
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -70,14 +50,32 @@ const LoginScreen: React.FC = () => {
     <Container>
       <Title>CheriC</Title>
       <Button onPress={signInWithGoogle}>
-        <Image
-          source={require('src/assets/google-logo.png')}
-          style={{ width: 24, height: 23 }}
-        />
+        <GoogleIcon />
         <ButtonText>{`   `}Sign up with Google</ButtonText>
       </Button>
     </Container>
   );
 };
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.cherryRed_10};
+`;
+
+const Title = styled(H1)`
+  margin-top: 250px;
+  margin-bottom: 350px;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const Button = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 12px 32px;
+  border-radius: ${({ theme }) => theme.radius.l};
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const ButtonText = styled(Subtitle2)``;
 
 export default LoginScreen;
