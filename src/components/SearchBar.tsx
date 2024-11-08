66142710db3849e4ad67e057b838f10f133527bf
Minji Kim <minjikim2000@hotmail.com>
2024-11-09 01:31:41 +0900

    feat: add delete button #35

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,25 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { TextInput } from 'react-native';
-
-const SearchBarContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  border-radius: ${(props) => props.theme.radius.m};
-  padding: ${(props) => props.theme.spacing.s3} 0;
-  padding-left: ${(props) => props.theme.spacing.s4};
-  background-color: ${(props) => props.theme.colors.grey_4};
-`;
-
-const SearchTextInput = styled(TextInput)`
-  flex: 1;
-  margin-left: ${(props) => props.theme.spacing.s3};
-  font-size: ${({ theme }) => theme.fontSizes.button};
-  line-height: ${({ theme }) => theme.lineHeights.button};
-  font-family: ${({ theme }) => theme.fonts.regular};
-  color: ${(props) => props.theme.colors.redBlack};
-`;
-
+import { XIcon } from 'src/assets/icons/_index';
 interface SearchBarProps {
   filterText: string;
   setFilterText: (text: string) => void;
@@ -34,15 +16,40 @@ const SearchBar: React.FC<SearchBarProps> = ({
 }) => {
   return (
     <SearchBarContainer>
-      <Icon name="search" size={15} color="#413333" />
+      <Icon name='search' size={15} color='#413333' />
       <SearchTextInput
         placeholder={placeholder}
-        placeholderTextColor="#B0ABAB"
+        placeholderTextColor='#B0ABAB'
         value={filterText}
         onChangeText={setFilterText}
       />
+      <DeleteButton onPress={() => setFilterText('')}>
+        <XIcon width='12' height='12' />
+      </DeleteButton>
     </SearchBarContainer>
   );
 };
 
 export default SearchBar;
+
+const SearchBarContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing.s3} 0;
+  padding-left: ${({ theme }) => theme.spacing.s4};
+  border-radius: ${({ theme }) => theme.radius.m};
+  background-color: ${({ theme }) => theme.colors.grey_4};
+`;
+
+const SearchTextInput = styled(TextInput)`
+  flex: 1;
+  margin-left: ${({ theme }) => theme.spacing.s3};
+  font-size: ${({ theme }) => theme.fontSizes.button};
+  line-height: ${({ theme }) => theme.lineHeights.button};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.redBlack};
+`;
+
+const DeleteButton = styled.TouchableOpacity`
+  padding-right: ${({ theme }) => theme.padding.m};
+`;
