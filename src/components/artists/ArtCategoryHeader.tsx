fc26d07b287d4b27c35fdfa9a2a9105520b40bb9
Minji Kim <minjikim2000@hotmail.com>
2024-11-12 01:58:14 +0900

    feat: add privateArtwork & exhibit options to reuse component #37

--- src/components/artists/ArtCategoryHeader.tsx
+++ src/components/artists/ArtCategoryHeader.tsx
@@ -4,8 +4,8 @@ import { Caption, H3, Subtitle2 } from 'src/styles/typography';
 import styled from 'styled-components/native';
 
 const ArtCategoryHeader: React.FC<{
-  categoryTitle: string;
-  categoryType: 'artwork' | 'artist';
+  categoryTitle?: string;
+  categoryType: 'artwork' | 'artist' | 'privateArtwork' | 'exhibit';
   style?: ViewStyle;
 }> = ({ categoryTitle, categoryType, style }) => {
   return (
@@ -15,7 +15,11 @@ const ArtCategoryHeader: React.FC<{
           source={
             categoryType === 'artist'
               ? require('src/assets/images/Character/artist_front.png')
-              : require('src/assets/images/CollectingPage/cherry.png')
+              : categoryType === 'artwork'
+                ? require('src/assets/images/CollectingPage/cherry.png')
+                : categoryType === 'privateArtwork'
+                  ? require('src/assets/images/CollectingPage/star.png')
+                  : require('src/assets/images/CollectingPage/door.png')
           }
           categoryType={categoryType}
         />
@@ -23,16 +27,28 @@ const ArtCategoryHeader: React.FC<{
       <TextWrapper>
         <TitleWrapper>
           <MainText>
-            {categoryType === 'artist' ? '신진 작가' : '작품'}
+            {categoryType === 'artist'
+              ? '신진 작가'
+              : categoryType === 'privateArtwork'
+                ? '소장작품'
+                : categoryType === 'exhibit'
+                  ? '컬렉션 전시'
+                  : '작품'}
           </MainText>
-          <CategoryWrapper>
-            <CategoryTitle>{categoryTitle}</CategoryTitle>
-          </CategoryWrapper>
+          {categoryType !== 'privateArtwork' && categoryType !== 'exhibit' && (
+            <CategoryWrapper>
+              <CategoryTitle>{categoryTitle}</CategoryTitle>
+            </CategoryWrapper>
+          )}
         </TitleWrapper>
         <SubText>
           {categoryType === 'artist'
             ? '신진 작가의 작품을 둘러보고 컬렉팅 해보세요!'
-            : '추천하는 작품을 둘러보고 컬렉팅 해보세요!'}
+            : categoryType === 'artwork'
+              ? '추천하는 작품을 둘러보고 컬렉팅 해보세요!'
+              : categoryType === 'privateArtwork'
+                ? '컬렉터들이 등록한 신규 소장작품을 둘러보세요!'
+                : '컬렉터들의 컬렉션 전시를 둘러보세요!'}
         </SubText>
       </TextWrapper>
     </HeaderWrapper>
@@ -48,7 +64,9 @@ const ImageWrapper = styled.View`
   margin-right: ${({ theme }) => theme.margin.s};
 `;
 
-const HeaderImage = styled.Image<{ categoryType: 'artwork' | 'artist' }>`
+const HeaderImage = styled.Image<{
+  categoryType: 'artwork' | 'artist' | 'privateArtwork' | 'exhibit';
+}>`
   width: ${({ categoryType }) => (categoryType === 'artist' ? '54px' : '64px')};
   height: ${({ categoryType }) =>
     categoryType === 'artist' ? '73px' : '74px'};
