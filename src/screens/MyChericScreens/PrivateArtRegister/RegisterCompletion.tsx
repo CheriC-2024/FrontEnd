ff6bdd7317aeb3a13e5c576f1ee1498285e54290
Minji Kim <minjikim2000@hotmail.com>
2024-11-24 16:37:39 +0900

    style: update register completion screen UI #43

--- src/screens/MyChericScreens/PrivateArtRegister/RegisterCompletion.tsx
+++ src/screens/MyChericScreens/PrivateArtRegister/RegisterCompletion.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Image } from 'react-native';
 import styled from 'styled-components/native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'src/store';
 import usePresignedUpload from 'src/api/hooks/usePresignedUpload';
 import { postPrivateArtwork } from 'src/api/artworkApi';
@@ -9,14 +9,42 @@ import { OwnArtCheck } from 'src/assets/images/MyCheric/_index';
 import { Btn, BtnText } from 'src/components/Button';
 import { Container } from 'src/styles/layout';
 import { Caption, H4 } from 'src/styles/typography';
+import { headerOptions } from 'src/navigation/UI/headerConfig';
+import { useNavigation } from '@react-navigation/native';
+import { resetArtwork } from 'src/slices/registerPrivateArtworkSlice';
+import TitleSubtitle from 'src/components/TitleSubtitle';
 
 const RegisterCompletion: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
   const { uploadFiles } = usePresignedUpload();
   const [loading, setLoading] = useState(true);
   const [postSuccess, setPostSuccess] = useState(false);
 
   const { artBasicInfo, imgUrls, artistName, price, isPriceOpen, fileUrl } =
     useSelector((state: RootState) => state.registerPrivateArtwork);
+
+  // 헤더 업데이트
+  useEffect(() => {
+    if (!loading) {
+      navigation.setOptions(
+        headerOptions(navigation, {
+          leftButtonType: 'text',
+          leftButtonText: ' ',
+          headerRightText: '나가기',
+          onHeaderRightPress: handleNext,
+          headerRightDisabled: false, // 로딩 완료 시 버튼 활성화
+        }),
+      );
+    }
+  }, [loading, navigation]);
+
+  const handleNext = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Tabs', params: { screen: 'Collecting' } }],
+    });
+  };
 
   const handleUploadAndPost = async () => {
     try {
@@ -84,6 +112,7 @@ const RegisterCompletion: React.FC = () => {
       console.log('POST 응답:', response);
 
       setPostSuccess(true);
+      dispatch(resetArtwork());
     } catch (error) {
       console.error('업로드 또는 POST 실패:', error);
     } finally {
@@ -106,15 +135,24 @@ const RegisterCompletion: React.FC = () => {
   if (postSuccess) {
     return (
       <Container>
-        <OwnArtCheck />
-        <H4>{artBasicInfo.name}</H4>
-        <H4>소장 작품 정보를</H4>
-        <H4>확인하고 있어요!</H4>
-        <Caption>
-          소장 인증이 완료되면{`\n`}컬렉터님께 알림으로 알려드릴게요!{`\n`}
-          *영업일 기준 하루~5일 가량 소요됩니다
-        </Caption>
-        <Btn style={{ marginHorizontal: 16, marginBottom: 60 }}>
+        <TitleSubtitle
+          titleLarge={'소장 작품 인증이 신청되었어요!'}
+          subtitle={'CheriC가 소장 정보를 확인한 후 승인해드릴게요!'}
+        />
+        <InfoContainer>
+          <OwnArtCheck />
+          <H4>{artBasicInfo.name}</H4>
+          <H4>소장 작품 정보를</H4>
+          <H4>확인하고 있어요!</H4>
+          <Caption style={{ textAlign: 'center', marginTop: 8 }}>
+            소장 인증이 완료되면{`\n`}컬렉터님께 알림으로 알려드릴게요!{`\n`}
+            *영업일 기준 하루~5일 가량 소요됩니다
+          </Caption>
+        </InfoContainer>
+        <Btn
+          onPress={handleNext}
+          style={{ marginHorizontal: 16, marginBottom: 60 }}
+        >
           <BtnText>네, 알겠어요!</BtnText>
         </Btn>
       </Container>
@@ -130,7 +168,6 @@ const RegisterCompletion: React.FC = () => {
 
 export default RegisterCompletion;
 
-// Styled components
 const LoadingContainer = styled.View`
   flex: 1;
   justify-content: center;
@@ -138,18 +175,11 @@ const LoadingContainer = styled.View`
   background-color: #fff;
 `;
 
-const SuccessContainer = styled.View`
-  flex: 1;
+const InfoContainer = styled.View`
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #fff;
-`;
-
-const SuccessMessage = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  color: #333;
-  text-align: center;
+  margin-top: 100px;
 `;
 
 const ErrorContainer = styled.View`
@@ -162,6 +192,6 @@ const ErrorContainer = styled.View`
 const ErrorMessage = styled.Text`
   font-size: 18px;
   font-weight: bold;
-  color: red;
+  color: #e52c32;
   text-align: center;
 `;
