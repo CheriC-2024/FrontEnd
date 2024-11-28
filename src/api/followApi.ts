0d6bdb5e0c0668ba21171f005373dda466077401
Minji Kim <minjikim2000@hotmail.com>
2024-11-28 23:55:01 +0900

    chore: update following related api endpoints #45

--- src/api/followApi.ts
+++ src/api/followApi.ts
@@ -2,12 +2,12 @@ import axiosInstance from './axiosInstance';
 
 // 팔로우 API 호출
 export const followUser = async (followedId: number) => {
-  const response = await axiosInstance.post(`/follow/${followedId}`);
+  const response = await axiosInstance.post(`/follows/${followedId}`);
   return response.data;
 };
 
 // 언팔로우 API 호출
 export const unfollowUser = async (followedId: number) => {
-  const response = await axiosInstance.delete(`/follow/${followedId}`);
+  const response = await axiosInstance.delete(`/follows/${followedId}`);
   return response.data;
 };
